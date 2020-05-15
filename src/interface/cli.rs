dd17522fb77fa7b04a9b53b98b72ba152d4fea4a
Tom Regan <tomregan@fastmail.fm>
2020-05-15 12:04:02 +0100

    refactor cli code into cli module

--- src/interface/cli.rs
+++ src/interface/cli.rs
@@ -1,7 +1,108 @@
 use crate::application::command::Command;
+use crate::configuration::Configuration;
 use clap::{App, AppSettings, Arg, SubCommand};
+use database::query::{list_fields, list_titles};
 
-pub fn parse_command_line() -> Command {
+pub struct Application {
+    cfg: Configuration,
+}
+
+impl Application {
+    pub fn new(cfg: Configuration) -> Self {
+        Application {
+            cfg,
+        }
+    }
+
+    pub fn run(self) -> Result<(), ()> {
+        let cmd = parse_command_line();
+        handle_command(self.cfg, cmd)
+    }
+}
+
+fn handle_command(cfg: Configuration, cmd: Command) -> Result<(), ()> {
+    // todo --- import command
+    match cmd {
+        Command::Config { .. } => handle_config_command(cfg, cmd),
+        Command::Fields => handle_fields_command(cfg, cmd),
+        Command::List { .. } => handle_list_command(cfg, cmd),
+        Command::Update => handle_update_command(cfg, cmd),
+        _ => {
+            println!("The application failed to complete, the reason is: unsupported command {:?}", cmd);
+            Err(())
+        },
+    }
+}
+
+fn handle_config_command(cfg: Configuration, cmd: Command) -> Result<(), ()> {
+    match cmd {
+        Command::Config { path: true, default: false } => {
+            println!("{}", cfg.get_source());
+            Ok(())
+        }
+        Command::Config { path: false, default: true } => {
+            println!("{}", &Configuration::default());
+            Ok(())
+        }
+        Command::Config { path: false, default: false } => {
+            println!("{}", cfg);
+            Ok(())
+        }
+        _ => Err(())
+    }
+}
+
+fn handle_fields_command(_cfg: Configuration, cmd: Command) -> Result<(), ()> {
+    match cmd {
+        Command::Fields => {
+            let available_fields = list_fields();
+            if available_fields.is_empty() {
+                println!("No available fields, is roots initialised?")
+            } else {
+                for f in available_fields {
+                    println!("{}", f);
+                }
+            }
+            Ok(())
+        }
+        _ => Err(())
+    }
+}
+
+fn handle_list_command(_cfg: Configuration, cmd: Command) -> Result<(), ()> {
+    match cmd {
+        Command::List { .. } => {
+            let available_titles = list_titles();
+            if available_titles.is_empty() {
+                println!("No titles to list, is roots initialised?");
+            } else {
+                for t in available_titles {
+                    println!("{}", t);
+                }
+            }
+            Ok(())
+        }
+        _ => Err(())
+    }
+}
+
+fn handle_update_command(_cfg: Configuration, cmd: Command) -> Result<(), ()> {
+    match cmd {
+        Command::Update => {
+            // scan the filesystem, cross-reference the database and ensure they're consistent
+            // use a 3p service to cross-ref metadata for existing titles
+            // offer to resolve any inconsistencies
+            let available_titles = list_titles();
+            if available_titles.is_empty() {
+                println!("No titles found, is roots initialised?");
+            }
+            Ok(())
+        }
+        _ => Err(())
+    }
+}
+
+fn parse_command_line() -> Command {
     let matches = App::new("root")
         .bin_name("root")
         .version(crate_version!())
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,8 @@ extern crate regex;
 extern crate serde;
 extern crate serde_yaml;
 
-use application::command::Command;
 use configuration::Configuration;
-use database::query::{list_fields, list_titles};
-use interface::cli::parse_command_line;
+use interface::cli;
 
 mod application;
 mod configuration;
@@ -19,46 +17,6 @@ mod interface;
 
 fn main() {
     let cfg = Configuration::new();
-    let cmd = parse_command_line();
-    match cmd {
-        Command::Config { path, default } => {
-            if path {
-                println!("{}", cfg.get_source())
-            } else if default {
-                println!("{}", &Configuration::default())
-            } else {
-                println!("{}", &cfg)
-            }
-        }
-        Command::Fields => {
-            let available_fields = list_fields();
-            if available_fields.is_empty() {
-                println!("No available fields, is roots initialised?")
-            } else {
-                for f in available_fields {
-                    println!("{}", f);
-                }
-            }
-        }
-        Command::List { .. } => {
-            let available_titles = list_titles();
-            if available_titles.is_empty() {
-                println!("No titles to list, is roots initialised?");
-            } else {
-                for t in available_titles {
-                    println!("{}", t);
-                }
-            }
-        }
-        Command::Update => {
-            // scan the filesystem, cross-reference the database and ensure they're consistent
-            // use a 3p service to cross-ref metadata for existing titles
-            // offer to resolve any inconsistencies
-            let available_titles = list_titles();
-            if available_titles.is_empty() {
-                println!("No titles found, is roots initialised?");
-            }
-        }
-        _ => unreachable!(),
-    }
+    let app = cli::Application::new(cfg);
+    app.run().ok();
 }
